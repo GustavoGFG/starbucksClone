b266d6e4a839c9cec2e11e7edebed3f20b063e4d
Gustavo Fernandes <goncalves_017@hotmail.com>
2023-10-31 15:41:55 -0300

    Fixing resizing issue with js resize listener

--- scrollTo.js
+++ scrollTo.js
@@ -1,21 +1,21 @@
 window.addEventListener('scroll', e => {
-  const headerHeight = document.getElementsByTagName('header')[0].clientHeight;
-  const titleContainer = document.getElementById('social-impact-title');
-  const asideLogoContainer = document.getElementById('aside-logo-container');
-  const footerHeight = document.getElementsByTagName('footer')[0].clientHeight;
-  const bodyheight = document.body.clientHeight;
-  const height = bodyheight - window.innerHeight - footerHeight;
-  const asideText = document.getElementById('aside-text-container');
+  let headerHeight = document.getElementsByTagName('header')[0].clientHeight;
+  let titleContainer = document.getElementById('social-impact-title');
+  let asideLogoContainer = document.getElementById('aside-logo-container');
+  let footerHeight = document.getElementsByTagName('footer')[0].clientHeight;
+  let bodyheight = document.body.clientHeight;
+  let height = bodyheight - window.innerHeight - footerHeight;
+  let asideText = document.getElementById('aside-text-container');
 
   if (window.innerWidth >= 995) {
     if (this.scrollY >= height) {
       asideLogoContainer.style.position = 'absolute';
-      asideLogoContainer.style.bottom = `${window.innerHeight / 2}px`;
       asideLogoContainer.style.left = '50%';
-    } else if (this.scrollY <= height) {
+      asideLogoContainer.style.bottom = `${window.innerHeight / 2}px`;
+    } else if (this.scrollY < height) {
       asideLogoContainer.style.position = 'fixed';
-      asideLogoContainer.style.bottom = `50%`;
       asideLogoContainer.style.left = '20%';
+      asideLogoContainer.style.bottom = `50%`;
     }
 
     if (this.scrollY >= height) {
@@ -25,7 +25,7 @@ window.addEventListener('scroll', e => {
       asideText.style.padding = '9% 15%';
       asideText.style.width = '100%';
       asideText.style.height = '5%';
-    } else if (this.scrollY <= height) {
+    } else if (this.scrollY < height) {
       asideText.style.position = 'fixed';
       asideText.style.bottom = `0`;
       // asideLogoContainer.style.left = '20%';
@@ -42,5 +42,24 @@ window.addEventListener('scroll', e => {
     titleContainer.style.width = '100%';
   } else if (this.scrollY < headerHeight) {
     titleContainer.style.position = 'static';
+  }
+});
+
+window.addEventListener('resize', e => {
+  let footerHeight = document.getElementsByTagName('footer')[0].clientHeight;
+  let bodyheight = document.body.clientHeight;
+  let height = bodyheight - window.innerHeight - footerHeight;
+  let asideLogoContainer = document.getElementById('aside-logo-container');
+  let asideText = document.getElementById('aside-text-container');
+  if (window.innerWidth <= 995) {
+    console.log(window.innerWidth);
+    asideLogoContainer.style.position = 'static';
+    asideText.style.position = 'static';
+  } else if (this.scrollY >= height) {
+    asideLogoContainer.style.position = 'absolute';
+    asideText.style.position = 'absolute';
+  } else {
+    asideLogoContainer.style.position = 'fixed';
+    asideText.style.position = 'fixed';
   }
 });
